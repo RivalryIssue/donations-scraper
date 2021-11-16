3fe56a1a18431ac5f8b6d2bab855121ba4f17bca
Rimaz Khan <rimazk@umich.edu>
2021-11-15 22:22:27 -0500

    add commas to daily money

--- scrape.py
+++ scrape.py
@@ -5,35 +5,38 @@ from bs4 import BeautifulSoup
 from json import dump
 from datetime import datetime
 
-def get_data(lanternurl: str, dailyurl:str ):
+
+def get_data(lanternurl: str, dailyurl: str):
 
     lantern_html = requests.get(f"{lanternurl}").text
     lantern_soup = BeautifulSoup(lantern_html, "html.parser")
 
-    money = lantern_soup.find_all(class_="single-project-top-dollar")[0].text.strip()
-
+    lantern_money = lantern_soup.find_all(
+        class_="single-project-top-dollar")[0].text.strip()
 
     daily_url = "https://give-webhooks.communityfunded.com/graphql"
 
-    payload="{\"query\":\"query publicStoryPageStats($storyId: Int!) {\\n  storyById(id: $storyId) {\\n    storyInitiativesByStoryId {\\n      nodes {\\n        id\\n        fundraisingGoalType\\n        calculatedGoal\\n        monetaryGoal\\n        totalNumberOfCalculatedItemsRaised\\n        totalAmountOfGifts\\n        totalNumberOfGifts\\n        totalNumberOfUniqueDonors\\n        calculatedFundraisingGoalName\\n        calculatedFundraisingGoalValue\\n        participantCountType\\n        participantGoal\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\",\"variables\":{\"storyId\":33211}}"
+    payload = "{\"query\":\"query publicStoryPageStats($storyId: Int!) {\\n  storyById(id: $storyId) {\\n    storyInitiativesByStoryId {\\n      nodes {\\n        id\\n        fundraisingGoalType\\n        calculatedGoal\\n        monetaryGoal\\n        totalNumberOfCalculatedItemsRaised\\n        totalAmountOfGifts\\n        totalNumberOfGifts\\n        totalNumberOfUniqueDonors\\n        calculatedFundraisingGoalName\\n        calculatedFundraisingGoalValue\\n        participantCountType\\n        participantGoal\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\",\"variables\":{\"storyId\":33211}}"
     headers = {
-    'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", daily_url, headers=headers, data=payload)
-    dailyMoney = round(float(response.json()["data"]["storyById"]["storyInitiativesByStoryId"]["nodes"][0]["totalAmountOfGifts"]))
+    response = requests.request(
+        "POST", daily_url, headers=headers, data=payload)
+    daily_money = round(float(response.json()[
+        "data"]["storyById"]["storyInitiativesByStoryId"]["nodes"][0]["totalAmountOfGifts"]))
 
     return {
         "data": {
-          "lantern": money,
-          "daily": f"${dailyMoney}",
+            "lantern": lantern_money,
+            "daily": "$" + "{:,}".format(daily_money),
         }
     }
 
 
 def main():
     lanternurl = "https://buckeyefunder.osu.edu/project/28518"
-    dailyurl=""
+    dailyurl = ""
     data = get_data(lanternurl, dailyurl)
     with open("data.json", "w") as f:
         dump(data, f, indent=2)
